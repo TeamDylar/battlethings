c48bb092f19eea42aa3cee38def0eb304d37d0af
scott.young <scott.young@appdynamics.com>
2014-02-07 20:31:05 -0800

    services

---
+++ app/scripts/services/admin.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('ss14Team113App')
+  .service('Admin', function Admin() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ app/scripts/services/game.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('ss14Team113App')
+  .service('Game', function Game() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
