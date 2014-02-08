e2f13fe371a736dae63310623fbe0c71a272b2a8
scott.young <scott.young@appdynamics.com>
2014-02-07 22:18:10 -0800

    Switch hint for karma, remove awesome things from tests.

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -2,9 +2,5 @@
 
 angular.module('ss14Team113App')
   .controller('GameCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      var i;
   });
