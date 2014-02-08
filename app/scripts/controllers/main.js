d423b6c46c5bca4ffa12e9e996d35076f8aa3e04
Dennis Woo <dwoodev@sent.com>
2014-02-08 11:11:45 -0800

    ugly, pulls static set of players from main, needs refactoring to right controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,12 @@
 
 angular.module('ss14Team113App')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.players = [
+      'player 1',
+      'player 2',
+      'player 3',
+      'player 4'
     ];
+    var dataRef = new Firebase("https://battlethings-dev1.firebaseio.com");
+    dataRef.set("I am now writing data into Firebase!");
   });
