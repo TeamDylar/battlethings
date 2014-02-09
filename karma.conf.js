c4875acb0a6c2d1c04bf13cb7d5204477f37e675
scott.young <scott.young@appdynamics.com>
2014-02-09 01:50:46 -0800

    Add local firebase and angularfire for testing.

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,8 @@ module.exports = function(config) {
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/jquery/jquery.js',
+      'app/components/firebase.js',
+      'app/components/angularfire.js',
       'app/components/lvl-drag-drop.js',
       'app/components/lvl-uuid.js',
       'app/scripts/*.js',
