3cfdc51a233285fde1d222a5498090e35ae8c2e6
scott.young <scott.young@appdynamics.com>
2014-02-15 15:11:17 -0800

    Add toggle for distributed execution with firebase, and synchronous execution for test or single player.

--- app/scripts/services/game.js
+++ app/scripts/services/game.js
@@ -3,7 +3,7 @@
 angular.module('ss14Team113App')
   .service('Game', function Game($q, $timeout) {
 
-    var dataUrl = 'https://battlethings-dev1.firebaseio.com',
+    var dataUrl = 'https://battlethings-dev1.firebaseio.com/',
         baseRef = new Firebase(dataUrl),
         gameRoom = 'test1vstest2', // $location.query(gameroom) ?
         gameRef = baseRef.child('game/' + gameRoom),
@@ -16,17 +16,13 @@ angular.module('ss14Team113App')
         callbackName = {
             UPDATE_BOARD: 'UPDATE_BOARD'
         },
-        shotDeferred;
+        shotDeferred, // deferred returned by a shot that resolves to shot response data
+        init = true, // boolean for initialization
+        distributed = false; // flag for Firebase (true) or local synchronous (false).;
 
-    //dataRef = new Firebase('https://SampleChat.BATTLETHINGS-DEV1.com/');
-    //
     // date ref for turn
     // - starts at 0, both create a random number, if 0 place number, if > num place num (so notify other player)
     //   larger numbers turn
-    //
-    // data ref for shot data
-    //
-    // data ref for shot response data
 
     this.messages = {
         STARTING: 'Game Starting',
@@ -52,14 +48,18 @@ angular.module('ss14Team113App')
         var Game = this,
             deferred = $q.defer();
 
-        $timeout(function() {
-            deferred.notify(Game.messages.WAITING);
-        }, 500);
+        if(distributed) {
+            console.log('distributed');
+        }
+        else { // test or single player
+            $timeout(function() {
+                deferred.notify(Game.messages.WAITING);
+            }, 500);
 
-        $timeout(function() {
-            deferred.resolve(Game.messages.STARTING);
-        }, 1000);
-
+            $timeout(function() {
+                deferred.resolve(Game.messages.STARTING);
+            }, 1000);
+        }
         return deferred.promise;
     };
 
@@ -67,19 +67,22 @@ angular.module('ss14Team113App')
       var Game = this,
           deferred = $q.defer();
 
-      //shotRef.set(shotPos, function(error) {
-      //  if (error) {console.log('Data could not be saved.' + error);} 
-      //  else {// No callback}
-      //});
+      if(distributed) {
+          console.log('distributed');
+          //shotRef.set(shotPos, function(error) {
+          //  if (error) {console.log('Data could not be saved.' + error);} 
+          //  else {// No callback}
+          //});
+      }
+      else { // test or single player
+          $timeout(function() {
+              deferred.notify(Game.messages.OPPONENTS_TURN);
+          }, 500);
 
-      $timeout(function() {
-          deferred.notify(Game.messages.OPPONENTS_TURN);
-      }, 500);
-
-      $timeout(function() {
-          deferred.resolve(Game.messages.YOUR_TURN);
-      }, 1000);
-
+          $timeout(function() {
+              deferred.resolve(Game.messages.YOUR_TURN);
+          }, 1000);
+      }
       return deferred.promise;
     };
 
@@ -88,31 +91,39 @@ angular.module('ss14Team113App')
           deferred = $q.defer(),
           shotPos = {row: row, col: col};
       shotDeferred = deferred;
+      if(distributed) {
+          //shotRef.set(shotPos, function(error) {
+          //  if (error) {console.log('Data could not be saved.' + error);} 
+          //  else {// No callback}
+          //});
+      }
+      else { // test or single player
+          $timeout(function() {
+              var data = {row: row, col: col};
+              angular.forEach(callbacks[callbackName.UPDATE_BOARD], function(fn) {
+                  fn.call(Game, data);
+              });
 
-      $timeout(function() {
-          var data = {row: row, col: col};
-          angular.forEach(callbacks[callbackName.UPDATE_BOARD], function(fn) {
-              fn.call(Game, data);
-          });
-
-      }, 200);
-
-      //shotRef.set(shotPos, function(error) {
-      //  if (error) {console.log('Data could not be saved.' + error);} 
-      //  else {// No callback}
-      //});
-
+          }, 200);
+      }
       return deferred.promise;
     };
 
-    this.respondToShot = function(data) {
-      //responseRef.set(shotPos, function(error) {
-      //  if (error) {console.log('Data could not be saved.' + error);} 
-      //  else {// No callback}
-      //});
-      shotDeferred.resolve(data);
-
-
+    /**
+     * respondToShot
+     *
+     * Send shot response information back to the opponent.
+     */
+    this.respondToShot = function(shotResponse) {
+        if(distributed) {
+            //responseRef.set(shotPos, function(error) {
+            //  if (error) {console.log('Data could not be saved.' + error);} 
+            //  else {// No callback}
+            //});
+        }
+        else {
+            shotDeferred.resolve(shotResponse);
+        }
     };
 
     this.getPlayer = function() {
@@ -124,27 +135,38 @@ angular.module('ss14Team113App')
     };
 
     turnRef.on('value', function(snapshot) {
+        if(!init){return;}
+        if(distributed) {
+            //angular.forEach(callbacks[callbackName.UPDATE_BOARD], function(fn) {
+            //    fn.call(Game, data);
+            //});
+        }
+        else { // test or single player
+            console.log("Error - shouldn't recieve shotRef.on in synchronous mode.");
+        }
     });
 
     shotRef.on('value', function(snapshot) {
-      //angular.forEach(callbacks[callbackName.UPDATE_BOARD], function(fn) {
-      //    fn.call(Game, data);
-      //});
+        if(!init){return;}
+        if(distributed) {
+            //angular.forEach(callbacks[callbackName.UPDATE_BOARD], function(fn) {
+            //    fn.call(Game, data);
+            //});
+        }
+        else { // test or single player
+            console.log("Error - shouldn't recieve shotRef.on in synchronous mode.");
+        }
     });
 
     responseRef.on('value', function(shotInfo) {
-        //shotDeferred.resolve(shotInfo);
+        if(!init){return;}
+        if(distributed) {
+            //shotDeferred.resolve(shotInfo);
+        }
+        else { // test or single player
+            console.log("Error - shouldn't recieve responseRef.on in synchronous mode.");
+        }
     });
 
-    //dataRef.on('value', function(snapshot) {
-    //$timeout(function() {
-    //    var data = {};
-    //    data.row = 1;
-    //    data.col = 1;
-    //    angular.forEach(callbacks[callbackName.UPDATE_BOARD], function(fn) {
-    //        fn.call(Game, data);
-    //    });
-    //}, 4000);
-    //});
-
+    init = false;
   });
